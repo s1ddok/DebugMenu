98738742c6e9acb1d45247600aa77a91eeff6df0
s1ddok <siddok@gmail.com>
2018-05-25 16:20:16 +0300

    small updates to bool option

--- Framework/Dotzu/Dotzu/BoolOptionCell.swift
+++ Framework/Dotzu/Dotzu/BoolOptionCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit.UITableViewCell
 
-public class BoolOptionCell: UITableViewCell, OptionCell {
+class BoolOptionCell: UITableViewCell, OptionCell {
     
     @IBOutlet weak var optionName: UILabel!
     @IBOutlet weak var `switch`: UISwitch!
@@ -18,11 +18,18 @@ public class BoolOptionCell: UITableViewCell, OptionCell {
         self.option?.value = sender.isOn
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        contentView.frame = UIEdgeInsetsInsetRect(contentView.frame, UIEdgeInsetsMake(-10, -10, -10, -10))
+    }
+    
     public func configure(for option: Option) {
         guard let boolOption = option as? BoolOption else {
             fatalError("Wrong option used for configuring cell")
         }
         
+        self.optionName.text = option.name
         self.switch.isOn = boolOption.value
         self.option = boolOption
     }
